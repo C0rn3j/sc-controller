de186a48e783bef4b677e95de0f88d0ed7b35567
micl <michaelwoodgerlloyd@gmail.com>
2026-07-17 21:27:15 +1000

    fix: Fix scc-osd-message doing nothing when run as an entry point (#104)

--- scc/bin/scc_osd_message.py
+++ scc/bin/scc_osd_message.py
@@ -8,26 +8,25 @@ def main() -> None:
 		print("\n*break*")
 		sys.exit(0)
 
-	if __name__ == "__main__":
-		signal.signal(signal.SIGINT, sigint)
+	signal.signal(signal.SIGINT, sigint)
 
-		import gi
+	import gi
 
-		gi.require_version("Gtk", "3.0")
-		gi.require_version("Rsvg", "2.0")
-		gi.require_version("GdkX11", "3.0")
+	gi.require_version("Gtk", "3.0")
+	gi.require_version("Rsvg", "2.0")
+	gi.require_version("GdkX11", "3.0")
 
-		from scc.tools import init_logging
+	from scc.tools import init_logging
 
-		init_logging()
+	init_logging()
 
-		from scc.osd.message import Message
+	from scc.osd.message import Message
 
-		m = Message()
-		if not m.parse_argumets(sys.argv):
-			sys.exit(1)
-		m.run()
-		sys.exit(m.get_exit_code())
+	m = Message()
+	if not m.parse_argumets(sys.argv):
+		sys.exit(1)
+	m.run()
+	sys.exit(m.get_exit_code())
 
 
 if __name__ == "__main__":
