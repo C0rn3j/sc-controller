b466edc43ecd3b53dc96f67e3ba56e6e9937e70e
Martin Rys <martin@rys.rs>
2026-08-17 01:13:17 +0200

    fix: Fix Kill Current Window by migrating from Python 2
    Migrate MenuData.from_args() to Python 3

--- scc/menu_data.py
+++ scc/menu_data.py
@@ -83,7 +83,7 @@ class MenuData:
 			raise ValueError("Not items")
 
 		# Rearange data into list of pair tuples
-		data = [(data[i * 2], data[(i * 2) + 1]) for i in range(len(data) / 2)]
+		data = [(data[i * 2], data[(i * 2) + 1]) for i in range(len(data) // 2)]
 
 		# Parse data
 		m = MenuData()
