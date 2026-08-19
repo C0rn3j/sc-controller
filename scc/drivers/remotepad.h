36e712705cb76d21804a415d1cca54d8b1849cc1
Martin Rys <martin@archlinux.org>
2026-08-19 18:41:14 +0200

    Fix: Fix remotepad switch fallthrough
    A switch had no break between RETRO_DEVICE_JOYPAD and RETRO_DEVICE_ANALOG, causing every button event to also execute the analog processing path.
    
    Also add explicit breaks elsewhere and fix up indenting.
    
    Co-Authored-By: Sergio Correia <sergio@correia.cc>

--- scc/drivers/remotepad.h
+++ scc/drivers/remotepad.h
@@ -20,8 +20,8 @@ struct remote_joypad_message {
 };
 
 typedef struct RemotePad {
-	Mapper*					mapper;
-	ControllerInput			input;
+	Mapper* mapper;
+	ControllerInput input;
 } RemotePad;
 
 
@@ -32,26 +32,26 @@ void remotepad_input(RemotePad* pad, struct remote_joypad_message* msg);
 // Buttons for the RetroPad (JOYPAD).
 // The placement of these is equivalent to placements on the Super Nintendo controller.
 // L2/R2/L3/R3 buttons correspond to the PS1 DualShock.
-#define RETRO_DEVICE_ID_JOYPAD_B			0
-#define RETRO_DEVICE_ID_JOYPAD_Y			1
-#define RETRO_DEVICE_ID_JOYPAD_SELECT		2
-#define RETRO_DEVICE_ID_JOYPAD_START		3
-#define RETRO_DEVICE_ID_JOYPAD_UP			4
-#define RETRO_DEVICE_ID_JOYPAD_DOWN			5
-#define RETRO_DEVICE_ID_JOYPAD_LEFT			6
-#define RETRO_DEVICE_ID_JOYPAD_RIGHT		7
-#define RETRO_DEVICE_ID_JOYPAD_A			8
-#define RETRO_DEVICE_ID_JOYPAD_X			9
-#define RETRO_DEVICE_ID_JOYPAD_L			10
-#define RETRO_DEVICE_ID_JOYPAD_R			11
-#define RETRO_DEVICE_ID_JOYPAD_L2			12
-#define RETRO_DEVICE_ID_JOYPAD_R2			13
-#define RETRO_DEVICE_ID_JOYPAD_L3			14
-#define RETRO_DEVICE_ID_JOYPAD_R3			15
+#define RETRO_DEVICE_ID_JOYPAD_B      0
+#define RETRO_DEVICE_ID_JOYPAD_Y      1
+#define RETRO_DEVICE_ID_JOYPAD_SELECT 2
+#define RETRO_DEVICE_ID_JOYPAD_START  3
+#define RETRO_DEVICE_ID_JOYPAD_UP     4
+#define RETRO_DEVICE_ID_JOYPAD_DOWN   5
+#define RETRO_DEVICE_ID_JOYPAD_LEFT   6
+#define RETRO_DEVICE_ID_JOYPAD_RIGHT  7
+#define RETRO_DEVICE_ID_JOYPAD_A      8
+#define RETRO_DEVICE_ID_JOYPAD_X      9
+#define RETRO_DEVICE_ID_JOYPAD_L      10
+#define RETRO_DEVICE_ID_JOYPAD_R      11
+#define RETRO_DEVICE_ID_JOYPAD_L2     12
+#define RETRO_DEVICE_ID_JOYPAD_R2     13
+#define RETRO_DEVICE_ID_JOYPAD_L3     14
+#define RETRO_DEVICE_ID_JOYPAD_R3     15
 
-#define RETRO_DEVICE_JOYPAD					1
-#define RETRO_DEVICE_ANALOG					5
-#define RETRO_DEVICE_INDEX_ANALOG_LEFT		0
-#define RETRO_DEVICE_INDEX_ANALOG_RIGHT		1
-#define RETRO_DEVICE_ID_ANALOG_X			0
-#define RETRO_DEVICE_ID_ANALOG_Y			1
+#define RETRO_DEVICE_JOYPAD             1
+#define RETRO_DEVICE_ANALOG             5
+#define RETRO_DEVICE_INDEX_ANALOG_LEFT  0
+#define RETRO_DEVICE_INDEX_ANALOG_RIGHT 1
+#define RETRO_DEVICE_ID_ANALOG_X        0
+#define RETRO_DEVICE_ID_ANALOG_Y        1
--- scc/drivers/remotepad_controller.c
+++ scc/drivers/remotepad_controller.c
@@ -17,20 +17,20 @@ static uint32_t next_id = 0;
 
 inline static SCButton libretro_button_to_sc_button(int id) {
 	switch (id) {
-	case RETRO_DEVICE_ID_JOYPAD_B:			return B_B;
-	case RETRO_DEVICE_ID_JOYPAD_Y:			return B_Y;
-	case RETRO_DEVICE_ID_JOYPAD_SELECT:		return B_BACK;
-	case RETRO_DEVICE_ID_JOYPAD_START:		return B_START;
-	case RETRO_DEVICE_ID_JOYPAD_A:			return B_A;
-	case RETRO_DEVICE_ID_JOYPAD_X:			return B_X;
-	case RETRO_DEVICE_ID_JOYPAD_L:			return B_LB;
-	case RETRO_DEVICE_ID_JOYPAD_R:			return B_RB;
-	case RETRO_DEVICE_ID_JOYPAD_L2:			return B_LT;
-	case RETRO_DEVICE_ID_JOYPAD_R2:			return B_RT;
-	case RETRO_DEVICE_ID_JOYPAD_L3:			return B_LSTICKPRESS;
-	case RETRO_DEVICE_ID_JOYPAD_R3:			return B_RSTICKPRESS;
-	default:
-		return 0;
+		case RETRO_DEVICE_ID_JOYPAD_B:      return B_B;
+		case RETRO_DEVICE_ID_JOYPAD_Y:      return B_Y;
+		case RETRO_DEVICE_ID_JOYPAD_SELECT: return B_BACK;
+		case RETRO_DEVICE_ID_JOYPAD_START:  return B_START;
+		case RETRO_DEVICE_ID_JOYPAD_A:      return B_A;
+		case RETRO_DEVICE_ID_JOYPAD_X:      return B_X;
+		case RETRO_DEVICE_ID_JOYPAD_L:      return B_LB;
+		case RETRO_DEVICE_ID_JOYPAD_R:      return B_RB;
+		case RETRO_DEVICE_ID_JOYPAD_L2:     return B_LT;
+		case RETRO_DEVICE_ID_JOYPAD_R2:     return B_RT;
+		case RETRO_DEVICE_ID_JOYPAD_L3:     return B_LSTICKPRESS;
+		case RETRO_DEVICE_ID_JOYPAD_R3:     return B_RSTICKPRESS;
+		default:
+			return 0;
 	}
 }
 
@@ -40,73 +40,73 @@ void remotepad_input(RemotePad* pad, struct remote_joypad_message* msg) {
 	// LOG("on_data_ready %i %i %i %i", msg->device, msg->index, msg->id, msg->state);
 
 	if (sizeof(SCButton) != sizeof(uint32_t)) {
-		printf("sizeof(SCButton) != sizeof(uint32_t): %zu != %zu\n",
-				sizeof(SCButton), sizeof(uint32_t));
+		printf("sizeof(SCButton) != sizeof(uint32_t): %zu != %zu\n", sizeof(SCButton), sizeof(uint32_t));
 	}
 
 	switch (msg->device) {
-	case RETRO_DEVICE_JOYPAD:
-		b = libretro_button_to_sc_button(msg->id);
-		if (b != 0) {
-			if (msg->state)
-				pad->input.buttons |= b;
-			else
-				pad->input.buttons &= ~b;
-		}
-
-		switch (msg->id) {
-		case RETRO_DEVICE_ID_JOYPAD_UP:
-			pad->input.dpad_y = (msg->state) ? STICK_PAD_MAX : 0;
-			break;
-		case RETRO_DEVICE_ID_JOYPAD_DOWN:
-			pad->input.dpad_y = (msg->state) ? STICK_PAD_MIN : 0;
-			break;
-		case RETRO_DEVICE_ID_JOYPAD_LEFT:
-			pad->input.dpad_x = (msg->state) ? STICK_PAD_MIN : 0;
-			break;
-		case RETRO_DEVICE_ID_JOYPAD_RIGHT:
-			pad->input.dpad_x = (msg->state) ? STICK_PAD_MAX : 0;
-			break;
-		case RETRO_DEVICE_ID_JOYPAD_L2:
-			pad->input.ltrig = (msg->state) ? TRIGGER_MAX : 0;
-			break;
-		case RETRO_DEVICE_ID_JOYPAD_R2:
-			pad->input.rtrig = (msg->state) ? TRIGGER_MAX : 0;
-			break;
-		case RETRO_DEVICE_ID_JOYPAD_SELECT:
-		case RETRO_DEVICE_ID_JOYPAD_START:
-			// If both start+select are pressed, "C" button is emulated
-			if ((pad->input.buttons & (B_BACK | B_START)) == (B_BACK | B_START)) {
-				pad->input.buttons &= ~(B_BACK | B_START);
-				pad->input.buttons |= B_C;
-			} else {
-				pad->input.buttons &= ~B_C;
+		case RETRO_DEVICE_JOYPAD:
+			b = libretro_button_to_sc_button(msg->id);
+			if (b != 0) {
+				if (msg->state)
+					pad->input.buttons |= b;
+				else
+					pad->input.buttons &= ~b;
 			}
-		}
-
-	case RETRO_DEVICE_ANALOG:
-		switch (msg->index) {
-		case RETRO_DEVICE_INDEX_ANALOG_LEFT:
 			switch (msg->id) {
-			case RETRO_DEVICE_ID_ANALOG_X:
-				pad->input.lstick_x = (AxisValue)msg->state;
-				break;
-			case RETRO_DEVICE_ID_ANALOG_Y:
-				pad->input.lstick_y = -(AxisValue)msg->state;
-				break;
+				case RETRO_DEVICE_ID_JOYPAD_UP:
+					pad->input.dpad_y = (msg->state) ? STICK_PAD_MAX : 0;
+					break;
+				case RETRO_DEVICE_ID_JOYPAD_DOWN:
+					pad->input.dpad_y = (msg->state) ? STICK_PAD_MIN : 0;
+					break;
+				case RETRO_DEVICE_ID_JOYPAD_LEFT:
+					pad->input.dpad_x = (msg->state) ? STICK_PAD_MIN : 0;
+					break;
+				case RETRO_DEVICE_ID_JOYPAD_RIGHT:
+					pad->input.dpad_x = (msg->state) ? STICK_PAD_MAX : 0;
+					break;
+				case RETRO_DEVICE_ID_JOYPAD_L2:
+					pad->input.ltrig = (msg->state) ? TRIGGER_MAX : 0;
+					break;
+				case RETRO_DEVICE_ID_JOYPAD_R2:
+					pad->input.rtrig = (msg->state) ? TRIGGER_MAX : 0;
+					break;
+				case RETRO_DEVICE_ID_JOYPAD_SELECT:
+				case RETRO_DEVICE_ID_JOYPAD_START:
+					// If both start+select are pressed, "C" button is emulated
+					if ((pad->input.buttons & (B_BACK | B_START)) == (B_BACK | B_START)) {
+						pad->input.buttons &= ~(B_BACK | B_START);
+						pad->input.buttons |= B_C;
+					} else {
+						pad->input.buttons &= ~B_C;
+					}
+					break;
 			}
 			break;
-		case RETRO_DEVICE_INDEX_ANALOG_RIGHT:
-			switch (msg->id) {
-			case RETRO_DEVICE_ID_ANALOG_X:
-				pad->input.rstick_x = (AxisValue)msg->state;
-				break;
-			case RETRO_DEVICE_ID_ANALOG_Y:
-				pad->input.rstick_y = -(AxisValue)msg->state;
-				break;
+		case RETRO_DEVICE_ANALOG:
+			switch (msg->index) {
+				case RETRO_DEVICE_INDEX_ANALOG_LEFT:
+					switch (msg->id) {
+						case RETRO_DEVICE_ID_ANALOG_X:
+							pad->input.lstick_x = (AxisValue)msg->state;
+							break;
+						case RETRO_DEVICE_ID_ANALOG_Y:
+							pad->input.lstick_y = -(AxisValue)msg->state;
+							break;
+					}
+					break;
+				case RETRO_DEVICE_INDEX_ANALOG_RIGHT:
+					switch (msg->id) {
+						case RETRO_DEVICE_ID_ANALOG_X:
+							pad->input.rstick_x = (AxisValue)msg->state;
+							break;
+						case RETRO_DEVICE_ID_ANALOG_Y:
+							pad->input.rstick_y = -(AxisValue)msg->state;
+							break;
+					}
+					break;
 			}
 			break;
-		}
 	}
 
 	pad->mapper->input(pad->mapper, &pad->input);
