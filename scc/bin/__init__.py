f261a8b96478af8d877f8e73963906b9b5fd755d
Martin Rys <martin@archlinux.org>
2026-08-25 23:29:16 +0200

    feat: Enable Python warnings module

--- scc/bin/__init__.py
+++ scc/bin/__init__.py
@@ -1,1 +1,13 @@
 """SC-Controller executables."""
+
+import logging
+import os
+import sys
+
+# https://docs.python.org/3/library/warnings.html
+logging.captureWarnings(capture=True)
+if not sys.warnoptions:
+	import warnings
+
+	warnings.simplefilter("default")
+	os.environ["PYTHONWARNINGS"] = "default"  # Also affect subprocesses
