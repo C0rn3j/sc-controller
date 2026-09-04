15e84f3a0b84c41209ed71475c8b7f8694fca863
Martin Rys <martin@archlinux.org>
2026-09-04 22:40:20 +0200

    fix(gtk4): Migrate `Gtk.IconTheme.get_default()

--- scc/bin/sc_controller.py
+++ scc/bin/sc_controller.py
@@ -26,8 +26,6 @@ def main() -> None:
 
 	glades = os.path.join(get_share_path(), "glade")
 	images = os.path.join(get_share_path(), "images")
-	if Gtk.IconTheme.get_default():
-		Gtk.IconTheme.get_default().append_search_path(images)
 	# GObject.threads_init()
 
 	from scc.gui.app import App
