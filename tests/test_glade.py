fa69265ed1f715d0dcd68ec28ede2179add23e33
Martin Rys <martin@archlinux.org>
2026-09-04 23:09:57 +0200

    Test for GTK4 UI files too

--- tests/test_glade.py
+++ tests/test_glade.py
@@ -29,18 +29,18 @@ def _get_files():
 			filename = os.path.join(path, f)
 			if os.path.isdir(filename):
 				recursive(filename)
-			elif filename.endswith(".glade"):
+			elif filename.endswith((".glade", ".ui")):
 				rv.append(filename)
 
 	recursive("glade/")
 	return sorted(rv)
 
 
-def _check_ids(el, filename, parent_id):
+def _check_ids(el, filename, parent_id) -> None:
 	"""Recursively walks through tree and check if every object has ID"""
 	for child in el:
 		if child.tag == "object":
-			msg = "Widget has no ID in %s; class %s; Parent id: %s" % (filename, child.attrib["class"], parent_id)
+			msg = "Widget has no ID in {}; class {}; Parent id: {}".format(filename, child.attrib["class"], parent_id)
 			assert child.attrib.get("id"), msg
 			for subel in child:
 				if subel.tag == "child":
@@ -48,14 +48,14 @@ def _check_ids(el, filename, parent_id):
 
 
 class TestGlade:
-	"""Tests every glade file in glade/ directory (and subdirectories) for known
-	problems that may cause GUI to crash in some environments.
+	"""Tests every glade file in glade/ directory (and subdirectories) for known problems that may cause GUI to crash in some environments.
 
 	(one case on one environment so far)
 	"""
 
-	def test_every_widget_has_id(self):
+	def test_every_widget_has_id(self) -> None:
 		"""Tests if every defined widget has ID.
+
 		Dummy widgets without ID are OK, in theory, but Ubuntu version
 		of libglade crashes witht them :(
 		"""
