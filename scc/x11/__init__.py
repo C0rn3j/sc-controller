81c506de327480b1409d3a130ec283f48e359454
Martin Rys <martin@archlinux.org>
2026-09-06 15:14:19 +0200

    fix(gtk4): Migrate from `GdkX11.x11_get_default_xdisplay`

--- scc/x11/__init__.py
+++ scc/x11/__init__.py
@@ -3,4 +3,14 @@
 Daemon-related stuff that really needs X server to work.
 """
 
-# there is also absolutely nothing useful in this file...
+
+def get_xdisplay():
+	"""Return the Xlib display used by GTK4, or None on non-X11 backends."""
+	from gi.repository import Gdk, GdkX11
+
+	from scc.lib import xwrappers as X
+
+	display = Gdk.Display.get_default()
+	if not isinstance(display, GdkX11.X11Display):
+		return None
+	return X.Display(hash(display.get_xdisplay()))
