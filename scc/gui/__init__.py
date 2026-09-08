4ce8ffcc19087bf28624f2c3af238c6f9592ca1d
Martin Rys <martin@archlinux.org>
2026-09-08 17:45:27 +0200

    chore(gtk4): Silence annoying Gtk slider CSS warning

--- scc/gui/__init__.py
+++ scc/gui/__init__.py
@@ -1,3 +1,5 @@
+from gi.repository import GLib
+
 from scc.constants import SCButtons
 
 BUTTON_ORDER = (
@@ -18,3 +20,24 @@ BUTTON_ORDER = (
 	SCButtons.RGRIP,
 	SCButtons.LGRIP,
 )
+
+def log_writer(log_level, fields, n_fields, user_data):
+	message = GLib.log_writer_format_fields(
+		log_level,
+		fields,
+		False,
+	)
+
+	# https://gitlab.gnome.org/GNOME/gtk/-/work_items/4446
+	if "(slider) reported min width -2, but sizes must be >= 0" in message \
+	or "(slider) reported min height -2, but sizes must be >= 0" in message:
+		return GLib.LogWriterOutput.HANDLED
+
+	return GLib.log_writer_default(
+		log_level,
+		fields,
+		user_data,
+	)
+
+
+GLib.log_set_writer_func(log_writer, None)
