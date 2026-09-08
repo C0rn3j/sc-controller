75c89ebb7c7f3ce9166b451f8cfaa585b2225224
Martin Rys <martin@archlinux.org>
2026-09-08 16:10:01 +0200

    chore: Shut up asyncio deprecation warnings

--- scc/bin/__init__.py
+++ scc/bin/__init__.py
@@ -10,4 +10,22 @@ if not sys.warnoptions:
 	import warnings
 
 	warnings.simplefilter("default")
-	os.environ["PYTHONWARNINGS"] = "default"  # Also affect subprocesses
+	# 3.14 deprecated this and it is to be removed in 3.16
+	# Shut it up meanwhile since it's not our problem to solve
+	warnings.filterwarnings(
+		"ignore",
+		message=r"'asyncio\.AbstractEventLoopPolicy' is deprecated.*",
+		category=DeprecationWarning,
+		module=r"gi\.events",
+	)
+	warnings.filterwarnings(
+		"ignore",
+		message=r"'asyncio\.get_event_loop_policy' is deprecated.*",
+		category=DeprecationWarning,
+		module=r"gi\.events",
+	)
+
+	# Apply the same for subprocesses
+	os.environ["PYTHONWARNINGS"] = (
+		"default,ignore:'asyncio.AbstractEventLoopPolicy' is deprecated:DeprecationWarning:gi.events,ignore:'asyncio.get_event_loop_policy' is deprecated:DeprecationWarning:gi.events"
+	)
